682df5eb031404dc3dbbf1aec4e996d451b06753
Nicole Hategekimana <nicoleha127@gmail.com>
2018-12-21 14:30:12 +0200

    javascript almost done

--- scripts.js
+++ scripts.js
@@ -1,92 +1,35 @@
-function result(event) {
-    var questionOne = $("input:radio[name=questionOne]:checked").val();
-    var questionTwo =$("input:radio[name=questionTwo]:checked").val();
-    var questionThree = $("input:radio[name=questionThree]:checked").val();
-    var questionFour =$("input:radio[name=questionFour]:checked").val();
-    var questionFive = $("input:radio[name=questionFive]:checked").val();
-    var questionSix =$("input:radio[name=questionSix]:checked").val();
-    var questionSeven = $("input:radio[name=questionSeven]:checked").val();
-    var questionEight = $("input:radio[name=questionEight]:checked").val();
-    var questionNine = $("input:radio[name=questionNine]:checked").val();
-    var questionTen = $("input:radio[name=questionTen]:checked").val();
-    var questionEleven = $("input:radio[name=questionEleven]:checked").val();
-    var questionTwelve = $("input:radio[name=questionTwelve]:checked").val();
-    var questionThirteen = $("input:radio[name=questionThirteen]:checked").val();
-    var questionFourteen = $("input:radio[name=questionFourteen]:checked").val();
-    var questionFifteen = $("input:radio[name=questionFifteen]:checked").val();
-    var questionSixteen = $("input:radio[name=questionSixteen]:checked").val();
-    var questionSeventeen =$("input:radio[name=questionSeventeen]:checked").val();
-    var questionEighteen = $("input:radio[name=questionEighteen]:checked").val();
-    var questionNineteen = $("input:radio[name=questionNineteen]:checked").val();
-    var questionTwenty = $("input:radio[name=questionTwenty]:checked").val();
+var answers = ["b", "c", "b", "b", "a", "a", "c", "c", "c", "a","b","a","a","c","d","c","d","b","b","c"]
+    tot = answers.length;
 
-    var count = 0;
-  
-    if (questionOne == "b") {
-      count += 1;
+function getCheckedValue(radioName) {
+    var radios = document.getElementsByName(radioName); // Get radio group by-name
+    for (var y = 0; y < radios.length; y++) {
+        if (radios[y].checked) return radios[y].value; // return the checked value
     }
-    if (questionTwo == "c") {
-      count += 1;
-    }
-    if (questionThree == "b") {
-      count += 1;
-    }
-    if (questionFour == "b") {
-      count += 1;
-    }
-    if (questionFive == "a") {
-      count += 1;
-    }
-    if (questionSix == "a") {
-      count += 1;
-    }
-    if (questionSeven == "c") {
-      count += 1;
-    }
-    if (questionEight == "c") {
-      count += 1;
-    }
-    if (questionNine == "c") {
-      count += 1;
-    }
-    if (questionTen == "a") {
-      count += 1;
-    }
-    if (questionEleven == "b") {
-    }
-    if (questionTwelve == "a") {
-      count += 1;
-    }
-    if (questionThirteen == "a") {
-      count += 1;
-    }
-    if (questionFourteen == "c") {
-      count += 1;
-    }
-    if (questionFifteen == "d") {
-      count += 1;
-    }
-    if (questionSixteen == "c") {
-      count += 1;
-    }
-    if (questionSeventeen == "d") {
-      count += 1;
-    }
-    if (questionEighteen == "b") {
-      count += 1;
-    }
-    if (questionNineteen == "b") {
-      count += 1;
-    }
-    if (questionTwenty == "c") {
-      count += 1;
-    }
-    document.getElementById("marks").styles.visibility = "visible";
-  
-    document.getElementById("mark").innerHTML ="You got " + count + "/20";
-  
-    $("#button").hide();
-    $("#quiz").hide();
-    $("#mark").show();
-    event.preventDefault();
-  }
+}
+
+function getScore() {
+    var score = 0;
+    debugger
+    for (var i = 0; i < tot; i++)
+        if (getCheckedValue("question" + i) === answers[i]) score += 1; // increment only
+    return score;
+}
+
+$(document).ready(function() {
+    $('form').show()
+    $('.result').hide()
+    $('form').submit(function(event) {
+        event.preventDefault();
+        $('form').hide();
+        $('.result').show()
+        returnScore();
+    })
+})
+
+
+
+function returnScore() {
+    // alert("Your score is " + getScore() + "/" + tot);
+    document.getElementById('marks').innerHTML = "Your score is " + "<br>" + getScore() + "/" + tot;
+}
